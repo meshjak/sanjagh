d8e0c5fe8e4b3795039127e529d6ad76cbe750e3
meshcoder <jakmesh96@gmail.com>
2021-08-11 18:59:19 +0430

    [v1/view]article_view__add_detail_about_article

--- views/article/view.php
+++ views/article/view.php
@@ -2,41 +2,37 @@
 
 use yii\helpers\Html;
 use yii\web\YiiAsset;
-use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Article */
 
 $this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Articles', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'مقالات', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 YiiAsset::register($this);
 ?>
 <div class="article-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'author_id',
-            'title',
-            'body:ntext',
-            'status',
-            'created_at',
-        ],
-    ]) ?>
-
+    <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h3><?= Html::encode($this->title) ?></h3>
+            <div>
+                <?= Html::a('بروزرسانی', ['update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
+                <?= Html::a('حذف', ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-sm btn-danger',
+                    'data' => [
+                        'confirm' => 'Are you sure you want to delete this item?',
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+        <div class="card-body">
+            <?= Html::encode($model->body) ?>
+        </div>
+        <div class="card-footer d-flex justify-content-between align-items-center">
+            <p class="mb-0">نویسنده: <span><?=  Html::encode($model->author->getFullname())  ?></span></p>
+            <div><span><?= Html::encode($model->relativeCreateTime())  ?></span></div>
+        </div>
+    </div>
 </div>
