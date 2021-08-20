cb342fb2b504d92a0f2d175e1dd4fff9f545d921
meshcoder <jakmesh96@gmail.com>
2021-08-21 04:00:34 +0430

    [v3/CRUD]model:Tag

---
+++ models/Tag.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tag".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string|null $name
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Article[] $articles
+ * @property User $user
+ */
+class Tag extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tag';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id'], 'integer'],
+            [['name'], 'string', 'max' => 40],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[ArticleTags]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticleTags()
+    {
+        return $this->hasMany(ArticleTag::className(), ['tag_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Articles]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['id' => 'article_id'])->viaTable('article_tag', ['tag_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
