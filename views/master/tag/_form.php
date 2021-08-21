1f61493b3f1c8969901249bcecaa823405706808
meshcoder <jakmesh96@gmail.com>
2021-08-21 15:37:26 +0430

    [v3/view]tag__form

--- views/master/tag/_form.php
+++ views/master/tag/_form.php
@@ -12,12 +12,10 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('ذخیره', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
