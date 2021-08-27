b7d9f60d19f20607d1eb76f22a088d332c1e4ac1
meshcoder <jakmesh96@gmail.com>
2021-08-27 17:48:30 +0430

    [v3/view]layout__add_logo_in_header

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -12,22 +12,31 @@
     $registerUrl = Url::toRoute('/register', true);
     $logoutUrl = Url::toRoute('/logout', true);
     ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top pb-0">
         <div class="container-lg">
-            <a class="navbar-brand" href="#">سنجاق</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <a href="<?= $homeUrl ?>" class="d-flex align-items-center p-1 text-secondary text-decoration-none
+                <?= Yii::$app->controller->getRoute() == 'article/index' ? 'border-bottom border-info' : '' ?>">
+                <span>
+                    <img class="round text-info" src="<?= Yii::getAlias('@web/image').'/logo/sanjagh.png' ?>" alt="avatar" height="30" width="30">
+                </span>
+                <div class="d-sm-flex d-none">
+                    سنجاق
+                </div>
+            </a>
+
+            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse d-lg-flex justify-content-lg-between" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="<?= $homeUrl ?>">خانه<span class="sr-only">(current)</span></a>
+                <ul class="navbar-nav mr-lg-3">
+                    <li class="nav-item">
+                        <a class="nav-link
+                        <?= Yii::$app->controller->getRoute() == 'site/contact' ? 'border-bottom border-info' : '' ?> "
+                           href="<?= $contactUsUrl ?>">ارتباط با ما</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= $contactUsUrl ?>">ارتباط با ما</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $aboutUrl ?>">درباره ما</a>
+                        <a class="nav-link <?= Yii::$app->controller->getRoute() == 'site/about' ? 'border-bottom border-info' : '' ?>"
+                           href="<?= $aboutUrl ?>">درباره ما</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav">
