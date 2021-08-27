76e895b97189bc403cd25db957d5f37784c4f8d9
meshcoder <jakmesh96@gmail.com>
2021-08-27 17:52:27 +0430

    [v3/view]article__add_search_button

--- views/article/_search.php
+++ views/article/_search.php
@@ -16,7 +16,11 @@ use yii\widgets\ActiveForm;
         'class' => 'form-horizontal',
     ]); ?>
 
-    <?= $form->field($model, 'searchstring', ['options' => ['class' => 'form-group mb-0']])->textInput(['maxlength' => true, 'placeholder' => 'جستجو ...', 'class'=> 'form-control form-control-sm'])->label(false) ?>
+    <div class="d-flex">
+        <?= $form->field($model, 'searchstring', ['options' => ['class' => 'form-group mb-0']])->textInput(['maxlength' => true, 'placeholder' => 'جستجو ...', 'class'=> 'form-control form-control-sm'])->label(false) ?>
+        <?= Html::submitButton('جستجو', ['class' => 'btn btn-sm btn-outline-info ml-2']) ?>
+    </div>
+
 
     <?php ActiveForm::end(); ?>
 
