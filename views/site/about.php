8ddc8f322bde4d169d2139c79618329b37c898ff
meshcoder <jakmesh96@gmail.com>
2021-08-29 20:04:08 +0430

    [v3/view]about__change_view

--- views/site/about.php
+++ views/site/about.php
@@ -2,17 +2,21 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Html;
 
 $this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+    <div class="row  d-flex justify-content-center">
+        <div class="col-11 col-lg-8">
+            <div class="card mt-5 position-relative rounded-0 border-dark shadow-md">
+                <img class="round text-info position-absolute" style="top: -1.75rem; right: -2.25rem"  src="<?= Yii::getAlias('@web/image').'/logo/sanjagh.png' ?>" alt="avatar" height="70" width="70">
+                <div class="card-body">
+                    <h3 class="font-weight-lighter ml-lg-4 ml-3 border-bottom border-info pb-1">سنجاق</h3>
+                    <p style="line-height: 2.5rem; text-align: justify;">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
